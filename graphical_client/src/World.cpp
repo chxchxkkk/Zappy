e9e1e16bb52a548a5a3860c428da4c4d52dc7477
arthur <arthur.teisseire@epitech.eu>
2019-06-04 17:30:05 +0200

    Add plv to actions

--- graphical_client/src/World.cpp
+++ graphical_client/src/World.cpp
@@ -12,5 +12,6 @@ World::World()
     actions = {
         {"pnw", [this](const std::vector<std::string> &input) { playerManager.pnw(input); }},
         {"ppo", [this](const std::vector<std::string> &input) { playerManager.ppo(input); }},
+        {"plv", [this](const std::vector<std::string> &input) { playerManager.plv(input); }},
     };
 }
