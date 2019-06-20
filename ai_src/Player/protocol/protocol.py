adeeb02118301b3f1d68771dd2f2df313b772f5f
Arthamios <adrien.fabre@epitech.eu>
2019-06-20 22:36:55 +0200

    FoodBehaviour : find_action_to_food return a list of instruction leading to nearest food, but for now it only goes forward

--- ai_src/Player/protocol/protocol.py
+++ ai_src/Player/protocol/protocol.py
@@ -1,12 +1,9 @@
 import socket
+
+from Player.Pendings import Action
 from ..Resource import Resource
 
 RECV_SIZE = 4096
-
-
-def forward(sock: socket.socket):
-    msg = str.encode("Forward\n")
-    sock.send(msg)
 
 
 def get_socket(port: int, host_name: str):
@@ -20,51 +17,67 @@ def send_team_name(sock: socket.socket, team_name: str):
     sock.send(msg)
 
 
+def forward(sock: socket.socket):
+    msg = str.encode("Forward\n")
+    sock.send(msg)
+    return Action.FORWARD
+
+
 def turn_right(sock: socket.socket):
     msg = str.encode("Right\n")
     sock.send(msg)
+    return Action.RIGHT
 
 
 def turn_left(sock: socket.socket):
     msg = str.encode("Left\n")
     sock.send(msg)
+    return Action.LEFT
 
 
 def get_inventory(sock: socket.socket):
     msg = str.encode("Inventory\n")
     sock.send(msg)
+    return Action.INVENTORY
 
 
 def send_broadcast(sock: socket.socket, message: str):
     msg = str.encode(message + "\n")
     sock.send(msg)
+    return Action.BROADCAST
 
 
 def fork(sock: socket.socket):
     msg = str.encode("Fork\n")
     sock.send(msg)
+    return Action.FORK
 
 
 def eject(sock: socket.socket):
     msg = str.encode("Eject\n")
     sock.send(msg)
+    return Action.EJECT
 
 
 def take_resource(sock: socket.socket, resource: Resource):
     msg = str.encode("Take " + resource.value + "\n")
     sock.send(msg)
+    return Action.TAKE
 
 
 def set_resource(sock: socket.socket, resource: Resource):
     msg = str.encode("Set " + str(resource) + "\n")
     sock.send(msg)
+    return Action.SET
 
 
 def incantation(sock: socket.socket):
     msg = str.encode("Incantation\n")
     sock.send(msg)
+    return Action.INCANTATION
 
 
 def look(sock: socket.socket):
     msg = str.encode("Look\n")
     sock.send(msg)
+    return Action.LOOK
