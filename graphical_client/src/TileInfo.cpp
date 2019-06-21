ff03cf9ebacb160b359bc2c1a895a4fa4ee4a7e1
arthur <arthur.teisseire@epitech.eu>
2019-06-21 10:50:35 +0200

    Update TileInfo positions

--- graphical_client/src/TileInfo.cpp
+++ graphical_client/src/TileInfo.cpp
@@ -13,11 +13,13 @@ TileInfo::TileInfo(Tile &tile)
 {
     if (!this->font.loadFromFile("assets/arial.ttf"))
         throw std::runtime_error("unable to load file assets/arial.ttf");
+    float x_pos = 1340;
     sf::Sprite background;
 
     background.setTexture(SingleTon<TextureLoader>::getInstance().getInstance("assets/menu_ba.jpg"));
-    background.setPosition(1040, 0);
+    background.setPosition(x_pos, 0);
     background.setColor(sf::Color(255, 255, 255, 150));
+    background.setScale(0.3f, 0.4f);
     this->sprites.push_back(background);
 
     auto inventory = tile.getContent();
@@ -30,10 +32,10 @@ TileInfo::TileInfo(Tile &tile)
 
         text += " : " + std::to_string(it.second);
         resource.setTexture(SingleTon<TextureLoader>::getInstance().getInstance(this->textureMap[it.first]));
-        resource.setPosition(1070, (i * 40) + 70);
+        resource.setPosition(x_pos + 30, (i * 40) + 70);
         resource.setScale(0.15f, 0.15f);
         quantity.setString(text);
-        quantity.setPosition(1150, (i * 40) + 70);
+        quantity.setPosition(x_pos + 110, (i * 40) + 70);
         quantity.setFillColor(sf::Color::Black);
         this->textInfo.push_back(quantity);
         this->sprites.push_back(resource);
